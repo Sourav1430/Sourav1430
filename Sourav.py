ff63a1e8439e9cac79a09e0f35f50daa9df52a37
Sourav Chakraborty <143569526+Sourav1430@users.noreply.github.com>
2023-09-27 05:07:34 +0000

    asdas

--- Sourav.py
+++ Sourav.py
@@ -4,3 +4,14 @@ from openpyxl.styles import PatternFill
 file = 'Book1.xlsx'
 wb = openpyxl.load_workbook(file)
 ws = wb.active
+
+ministry_column = ws['A']
+yellow_fill = PatternFill(start_color='FFFF00', end_color='FFFF00', fill_type='solid')
+
+for cell in ministry_column:
+    if cell.value == 'Rahul':
+        for row in ws.iter_rows(min_row=1, min_col=cell.column, max_col=cell.column):
+            for cell_in_col in row:
+                cell_in_col.fill = yellow_fill
+
+wb.save('Book2.xlsx')
